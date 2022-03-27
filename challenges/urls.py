5f16e521ec194ae3aaf48820f0103c04e9ce9c12
anandhulk <anandhulal7@gmail.com>
2022-03-27 11:08:12 +0530

    static files

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index),
+    path('',views.index,name="index"),
     path('<int:month>',views.m_int),
     path('<str:month>',views.m_challenges)
 ]
